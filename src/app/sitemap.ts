aac6cd89909bf1d4fc290e57fb91b595552e61d4
Joseph Wibowo <josephwibowo@gmail.com>
2026-01-25 16:54:09 -0800

    fix: force sitemap to be static for build export

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next';
 import { projects } from '@/data/projects';
 
+export const dynamic = 'force-static';
+
 export default function sitemap(): MetadataRoute.Sitemap {
     const baseUrl = 'https://josephwibowo.github.io';
 
