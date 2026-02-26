a6739690cdc4e9d694225f49c5d4a015de3c39db
Joseph Wibowo <josephwibowo@gmail.com>
2026-02-26 11:40:11 -0800

    verification

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/josephwibowo.github.io',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
